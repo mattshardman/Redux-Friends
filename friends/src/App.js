8053cd402036f026906bdbc0822f8a1a54c8fdae
mattshardman <mattshardman@gmail.com>
2019-03-07 10:36:19 +0000

    create-react-app, add dependencies and create redux store

---
+++ friends/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { createStore, applyMiddleware , compose} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import types from './constants';
+
+const customMiddlewareToSaveUserToke = store => next => action => {
+  if(action.types === types.LOGIN_SUCCESS) {
+    localStorage.setItem('userToken', action.payload);
+  }
+  next(action);
+}
+
+const store = createStore(
+  rootReducer, 
+  {}, 
+  compose(
+    applyMiddleware(thunk, customMiddlewareToSaveUserToke),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+      </Provider>
+    );
+  }
+}
+
+export default App;
